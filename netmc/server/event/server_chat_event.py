b1f963f2b84227119edef38f27b40251fd92d256
Choi-Jungwoo <jungwoo@cineuse.com>
2021-03-04 18:42:46 +0800

    update ServerChatEvent

--- netmc/server/event/server_chat_event.py
+++ netmc/server/event/server_chat_event.py
@@ -5,14 +5,31 @@ from ..player import Player
 
 @Event.config('ServerChatEvent')
 class ServerChatEvent(BaseEvent):
-    def __init__(self, player):
-        self.__player = Player(player)
+    def __init__(self, playerId, username, message, cancel, bChatById, toPlayerIds, players, messageValid):
+        self.__player = Player(playerId)
+        self.__username = username
+        self.__message = message
+        self.__player_list = toPlayerIds
+        self.chat_by_player = bChatById
+        self.cancel = cancel
 
     @property
     def player(self):
         # type: () -> Player
         return self.__player
 
-    @classmethod
-    def _serialize(cls, data):
-        return cls(data['playerId'])
+    @property
+    def username(self):
+        return self.__username
+
+    @property
+    def message(self):
+        return self.__message
+
+    @property
+    def player_list(self):
+        return self.__player_list
+
+    @player_list.setter
+    def player_list(self, value):
+        self.__player_list = [player.name for player in value]
