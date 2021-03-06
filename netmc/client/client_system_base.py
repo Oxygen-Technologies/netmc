e76cbfd2fcd2c8514ed3ccb4f198f5e2278898d6
aaron <aaron@cineuse.com>
2021-03-07 02:28:23 +0800

    实现了客户端与服务端快速通讯的基本逻辑

---
+++ netmc/client/client_system_base.py
@@ -0,0 +1,120 @@
+# coding=utf-8
+import mod.client.extraClientApi as clientApi
+from mod.client.system.clientSystem import ClientSystem
+from mod.client.clientEvent import ClientEvent
+
+
+class ClientSystemBase(ClientSystem):
+    """
+    此类作为Mod客户端系统的基类，旨在优化开发接口，降低开发的复杂度。主要从包含以下几个方面：
+
+    1. 引擎事件监听机制
+    2. UI集成机制
+    3. 方块事件监听注册机制
+    4. 自定义事件注册机制
+    5. 服务端数据获取机制
+
+    ## 引擎事件监听机制
+
+    原本监听自定义事件的方法
+    ```
+    self.ListenForEvent(MOD_NAME, SERVER_SYSTEM_NAME, ON_RETURN_PLAYER_CAMERAS, self, self.callback_function)
+    ```
+    优化后的方法
+    ```
+    @event()    # MOD_NAME, SERVER_SYSTEM_NAME 可作为参数传入event()
+    def on_return_player_cameras(data):
+        pass
+    ```
+    """
+
+    def __init__(self, namespace, system_name):
+        super(ClientSystemBase, self).__init__(namespace, system_name)
+
+        self.__server_data = {}
+        self._mod_name = None
+        self._server_system_name = None
+
+        self.__init_by_config()
+
+        self.player_id = clientApi.GetLocalPlayerId()
+
+        # 监听从服务端返回数据的Notify
+        self.ListenForEvent(self.mod_name, self.server_system_name, 'data_from_server', self, self._on_server_data_got)
+
+        self.game_comp = clientApi.GetEngineCompFactory().CreateGame(self.player_id)
+
+    def __init_by_config(self):
+        """
+        通过配置初始化
+        Returns:
+
+        """
+        import config
+        self._mod_name = config.MOD_NAME
+        self._server_system_name = config.SERVER_SYSTEM_NAME
+
+    @property
+    def mod_name(self):
+        if isinstance(self._mod_name, basestring):
+            return self._mod_name
+        raise ValueError(u'没有获取到mod_name的值, 请检查配置, 不存在或不是字符串...')
+
+    @property
+    def server_system_name(self):
+        if isinstance(self._server_system_name, basestring):
+            return self._server_system_name
+        raise ValueError(u'没有获取到client_system_name的值, 请检查配置, 不存在或不是字符串...')
+
+    def exec_delay(self, delay, func, args):
+        self.game_comp.AddTimer(delay, func, args)
+
+    def popup_notice(self, msg, subtitle='', color=''):
+        """
+        代替原生的game_comp.SetPopupNotice方法
+        Args:
+            msg: 信息
+            subtitle: 子标题
+            color: 必须是RED ... 中的一个
+
+        Returns:
+
+        """
+        self.game_comp.SetPopupNotice(color + msg, subtitle)
+
+    def from_server(self, signore, data=None, delay=0.1):
+        """
+        此函数用于与从服务端获取数据，你需要在客户端实现一个名称与signore参数相同的函数，并用 @to_client() 装饰器装饰，
+        或是在装饰器参数中指明signore名称，如 @to_client('foo')
+        Args:
+            signore: string 信号名称
+            data: dict 需要传入服务端函数的名称
+            delay: float 延迟获取结果的时间，用于解决服务端延迟较大的问题，默认为0.1秒
+
+        Returns:
+
+        """
+        # 通过NotifyToServer向服务端发起请求
+        self.NotifyToServer('get_data_from_server', {'player_id': self.player_id, 'signore': signore, 'data': data})
+        # 从结果中获取信息
+        return self.__get_server_data(signore, delay)
+
+    def __get_server_data(self, signore, delay):
+        # TODO 将实现替换成延迟获取结果
+        retry_times = 10
+        while retry_times > 0:
+            res = self.__server_data.get(signore)
+            if res is not None:
+                return res
+            retry_times -= 1
+
+    def _on_server_data_got(self, data):
+        """
+        从服务端获取到数据的回调
+        Args:
+            data:
+
+        Returns:
+
+        """
+        self.__server_data.update(data)
---
+++ netmc/server/server_system_base.py
@@ -0,0 +1,132 @@
+# coding=utf-8
+from mod.server.system.serverSystem import ServerSystem
+
+
+class ServerSystemBase(ServerSystem):
+    """
+    此类作为Mod服务端系统的基类，旨在简化开发的复杂度。主要从包含以下几个方面：
+
+    1. 简化事件注册机制
+    2. 与客户端无感通信
+
+    ## 与客户端无感通信
+    当客户端向服务端发起Notify时，原本的方法是无法通过返回值来得到服务端的结果的。
+    改进后，客户端发起的查询类请求，服务端都会通过反向Notify的方式反馈给客户端
+
+    客户端代码
+    ```
+    def show_cctvs(self):
+        cctvs = self.from_server('get_cameras', {'player_id': self.player_id})
+        self.popup_notice(u'你拥有的监控探头有 {}'.format(cctvs))
+    ```
+
+    服务端代码
+    ```
+    @to_client()    # 如果客户端from_server函数的signore参数和此函数名不同，通过传入参数到 to_client 来完成匹配
+    def get_cctvs(self, data):
+        ...
+        cctvs = [...]
+        return cctvs
+    ```
+    """
+    event_list = {}
+    to_client_functions = {}
+
+    def __init__(self, namespace, system_name):
+        super(ServerSystemBase, self).__init__(namespace, system_name)
+
+        self._mod_name = None
+        self._client_system_name = None
+
+        self.__init_by_config()
+
+        self.__register_events()
+        self.__init_to_client_pipeline()
+
+    def __init_by_config(self):
+        """
+        通过配置初始化
+        Returns:
+
+        """
+        import config
+        self._mod_name = config.MOD_NAME
+        self._client_system_name = config.CLIENT_SYSTEM_NAME
+
+    def __init_to_client_pipeline(self):
+        self.ListenForEvent(instance=self,
+                            namespace=self.mod_name,
+                            systemName=self.client_system_name,
+                            eventName='get_data_from_server',
+                            func=self.data_to_client)
+
+    @property
+    def mod_name(self):
+        if isinstance(self._mod_name, basestring):
+            return self._mod_name
+        raise ValueError(u'没有获取到mod_name的值, 请检查配置, 不存在或不是字符串...')
+
+    @property
+    def client_system_name(self):
+        if isinstance(self._client_system_name, basestring):
+            return self._client_system_name
+        raise ValueError(u'没有获取到client_system_name的值, 请检查配置, 不存在或不是字符串...')
+
+    def __register_events(self):
+        """自动注册时间装饰器装饰过的函数为服务端事件"""
+        for event_data in self.event_list.values():
+            event_data['instance'] = self
+            self.ListenForEvent(**event_data)
+
+    def destroy(self):
+        pass
+
+    def Destroy(self):
+        self.destroy()
+
+    @classmethod
+    def to_client(cls, alias=None):
+        """用于将方法转换为向客户端反馈数据的回调函数的装饰器"""
+
+        def inner(func):
+            new_name = alias or func.__name__
+            cls.to_client_functions[new_name] = func
+            return func
+
+        return inner
+
+    def __exec_signore(self, signore, data):
+        """
+        这个函数用于动态调用子类对象的方法
+        这个函数的实现借助了eval的特性，下面这段代码说明了它的可行性
+        ```
+        class A(object):
+            def x(self):
+                return eval('foo', globals(), {k: getattr(self, k) for k in dir(self)})
+
+        class B(A):
+            def foo(self):
+                print('ok')
+
+        b = B()
+        b.x()()
+        # 结果将成功打印出 ok
+        ```
+        """
+        func_name = self.to_client_functions.get(signore)
+        func = eval(func_name, globals(), {k: getattr(self, k) for k in dir(self)})
+        if hasattr(func, 'to_client'):
+            return func(data)
+
+    def data_to_client(self, signore_data):
+        player_id = signore_data.get('player_id')
+        signore = signore_data.get('signore')
+        data = signore_data.get('data')
+        result = self.__exec_signore(signore, data)
+        # 返回数据给客户端
+        self.NotifyToClient(player_id,
+                            '_on_server_data_got',
+                            result)
+
+    def __del__(self):
+        self.UnListenAllEvents()
